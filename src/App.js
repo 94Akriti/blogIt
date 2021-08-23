632ffff01d0f555c0c5f920711a820a1cb5f2f27
Akriti Yadav <94akritiyadav@gmail.com>
2021-08-23 10:48:46 +0530

    Functional and class component together

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
-import { render } from '@testing-library/react';
+//import { render } from '@testing-library/react';
 import React from 'react';
 //import logo from './logo.svg';
 import './App.css';
-
+import {Header} from './components/Header'
+import CHeader from './components/CHeader'
 class App extends React.Component{
   render(){
-    return <hi>This is Class component</hi>
+    return(
+      <div className="App">
+        <Header/> 
+        <CHeader/>
+      </div>
+      );
   }
 }
 
---
+++ src/components/Header.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export const Header=()=>{
+    return <h1>Hello Functional Component</h1>
+}
